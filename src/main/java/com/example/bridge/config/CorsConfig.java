1c72841f50a1a09cd0080abc8780563a66b2be82
Sowmiya0411 <ssow4661@gmail.com>
2026-07-08 16:12:24 +0530

    Update CorsConfig.java

--- src/main/java/com/example/bridge/config/CorsConfig.java
+++ src/main/java/com/example/bridge/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
-            "http://Sowmiya0411.github.io"
+            "https://sowmiya0411.github.io"
         ));
 
         configuration.setAllowedMethods(List.of(
